09b9b7678adec3df1dabef30a50d34b51502e9b5
SantiBox <salzate93@misena.edu.co>
2022-03-15 15:06:32 -0500

    Cambiamos el wandering

--- wandering.py
+++ wandering.py
@@ -1,12 +1,43 @@
 import random
  
 class  wandering:
-    def __init__(self, name):
+    def __init__(self, name, x=0, y=0):
         self.name = name
-       
+        self.y = y
+        self.x = x
+
+    def position (self):
+        return (self.x, self.y)
+
+    def distance_origin(self):
+        return (self.x**2 + self.y**2)**0.5
+
 class ComunWandering(wandering):
     def __init__(self, name):
         super().__init__(name)
        
     def walk(self):
-        return random.choice([(0,4),(0,-3),(3,0),(-3,0)])
+        dx, dy = random.choice([(0,4),(0,-3),(3,0),(-1,0)])
+        self.y += dy
+        self.x += dx
+        return [dx, dy]
+
+class RightWandering(wandering):
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def walk(self):
+        dx, dy = random.choice([(0,5),(5,0),(-1,0),(0,-1)])
+        self.y += dy
+        self.x += dx
+        return [dx, dy]
+
+class LeftWandering(wandering):
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def walk(self):
+        dx, dy = random.choice([(0,1),(1,0),(-5,0),(0,-5)])
+        self.y += dy
+        self.x += dx
+        return [dx, dy]
